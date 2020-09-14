0aff09dc74f1d45e964003f7ea7622a4d13c14d6
sPesce <63768994+sPesce@users.noreply.github.com>
2020-09-14 15:04:53 -0400

    Update solution.js

--- Javascript/Beginner Series #3 Sum of Numbers/solution.js
+++ Javascript/Beginner Series #3 Sum of Numbers/solution.js
@@ -7,3 +7,9 @@ function getSum(a, b) {
 
   return sum;
 }
+
+//Given two integers a and b, which can be positive or negative,
+//find the sum of all the numbers between including them too 
+//and return it. If the two numbers are equal return a or b.
+
+//Note: a and b are not ordered!
