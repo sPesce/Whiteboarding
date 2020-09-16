7f0f8f922ab6a2688d76b4a960fbd1dd26b63128
Steve Pesce <stevepesce879@gmail.com>
2020-09-16 14:56:23 -0400

    first java kata, title case

---
+++ Java/Title Case Tweets/solution.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+public class JadenCase {
+    //Title case string
+    public String toJadenCase(String phrase) {
+        if(phrase == null||phrase.isEmpty()||phrase.isBlank())
+            return null;
+        
+        int len = phrase.length();
+        char[] chars = new char[len];
+        chars[0] = Character.toUpperCase(phrase.charAt(0));
+        
+        for(int i = 1; i < len; i++)
+        {
+            final char current = phrase.charAt(i);
+            final char last = phrase.charAt(i-1);
+            if(Character.isLetter(last))
+                chars[i] = Character.toLowerCase(current);
+            else
+                chars[i] = Character.toUpperCase(current);                
+        }
+        return new String(chars); 
+    }
+}
+
+/*... Your task is to convert strings to how they would be 
+written by Jaden Smith. The strings are actual quotes from Jaden Smith, 
+but they are not capitalized in the same way he originally typed them. */
