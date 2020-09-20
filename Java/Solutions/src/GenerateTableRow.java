cbb9c77038bfa64f4b5f1822452092a19242e713
Steve Pesce <stevepesce879@gmail.com>
2020-09-20 16:04:22 -0400

    made html helper in java

---
+++ Java/Solutions/src/GenerateTableRow.java
@@ -0,0 +1,28 @@
+// -==========================================================================
+// Purpose: create the table rows in this projects readme
+//  --------------
+//  title: The first column of the table, what is this kata?
+//  solutionUrl: 2nd col, full github url to solution
+//  kataUrl: 3rd col, kata's full url on codewars
+//  --------------
+//  output: poorly formatted html to the console, but it saves some time
+//-===========================================================================
+public class GenerateTableRow {
+  public static String make(String title, String solutionUrl, String kataUrl)
+  {
+    String str = 
+    "<tr>\n" +
+      "<td><strong>" + title + "</strong></td>\n" +
+      "<td>\n" +
+        "<a href=\"" + solutionUrl + "\">\n" +
+        "solution</a>\n" +
+      "</td>\n" +
+      "<td>\n" +
+        "<a href=\"" + kataUrl + "\">\n" +
+        "kata\n" +
+        "</a>\n" +
+      "</td>\n" +
+    "</tr>\n";
+    return str;
+  }
+}
