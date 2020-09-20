a19f369843acb400e8c5514f75c5f5ff6e7e8cfc
Steve Pesce <stevepesce879@gmail.com>
2020-09-20 14:52:27 -0400

    refactored string ends with kata

--- Java/Solutions/src/StringEndsWith.java
+++ Java/Solutions/src/StringEndsWith.java
@@ -1,5 +1,6 @@
 public class StringEndsWith {
-  public static boolean solution(String str, String ending) { 
-    return ending.equals(str.substring( str.length() - ending.length()));
+  public static boolean solution(String str, String ending) {
+    if (ending.length() > str.length()) return false;
+    return str.equals(ending) || ending.equals(str.substring( str.length() - ending.length()));
   }
 }
