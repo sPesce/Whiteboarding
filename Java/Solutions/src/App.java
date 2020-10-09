6ddd22195bbbc59fb3563c2b6ffd2d4964992ee5
Steve Pesce <stevepesce879@gmail.com>
2020-10-09 18:00:35 -0400

    testing solution

--- Java/Solutions/src/App.java
+++ Java/Solutions/src/App.java
@@ -1,6 +1,6 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(GenerateTableRow.make("Is Panagram?","https://github.com/sPesce/Code_Wars_Katas/blob/master/Java/Solutions/src/PangramChecker.java","https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/java")); 
+        System.out.println(MiddleIndex.findEvenIndex(new int[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,20})); 
     }
 }
