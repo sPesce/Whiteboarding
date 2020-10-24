230f4bbbe7630f075acf950547ac18cb52cfa562
Steve Pesce <stevepesce879@gmail.com>
2020-10-24 15:46:54 -0400

    almost working, overlapping isnt passing

--- Java/Solutions/src/Interval.java
+++ Java/Solutions/src/Interval.java
@@ -1,25 +1,51 @@
+package cw;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Interval {
 
     public static int sumIntervals(int[][] intervals) {
-      if(intervals == null) return 0;
+      if( intervals == null || intervals.length == 0 ) return 0;
       
-      int start = Integer.MAX_VALUE;
-      int end = Integer.MIN_VALUE;
-      int total = 0;
+      int[][] values = new int[intervals.length][];
       
-      for(int[] interval : intervals)
-      {
-        if(interval[0] < start ) start = interval[0];
-        else if(interval[0] > end)
-        {
-          total += end - start;
-          start = interval[0];
-        }
+      int jMax = 0;
 
-        if(interval[1] > end) end = interval[1];
+      for(int i = 0; i < intervals.length ; i++)
+      {
+        //a: start of interval, b: end of it
+        final int a = intervals[i][0];
+        final int b = intervals[i][1];
+        System.out.println("Interval: [" + a + "," + b + "]" );
+        for(int j = 0; j <= i ; j++)
+        {
+          jMax = Integer.max(jMax,j);
+          //System.out.println("Values[" + j + "][0] = " + values[j][0]);
+          //System.out.println("Values[" + j + "] length : " + values[j].length);
+          System.out.println("Values: " + values[j]);
+          if(values[j] == null)
+          {
+            System.out.println("Hit empty");
+            System.out.println("Found empty values, setting " + a + "," + b);
+            values[j] = new int[] {a,b};
+            break;
+          }
+          //is a between values[j][0] and values[j][1]
+          else if( a >= values[j][0] && a < values[j][1])
+          {
+            if( b > values[j][1]) values[j][1] = b;
+            break;
+          }
+        } 
       }
-      total += end - start;
-      return total;
+      
+      int sum = 0;
+      for(int i = 0 ; i <= jMax ; i++)
+      {
+        System.out.println("Adding (" + values[i][1] + " - " + values[i][0] + ") to sum: " + sum);
+        sum += (values[i][1] - values[i][0]); 
+      }
+      return sum; 
     }
 }
