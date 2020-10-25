f62b7a91baf7820ca992e2b30aa08361afa13214
Steve Pesce <stevepesce879@gmail.com>
2020-10-25 15:46:23 -0400

    trying partially recursive solution

--- Java/Solutions/src/Strings.java
+++ Java/Solutions/src/Strings.java
@@ -2,16 +2,15 @@ import java.util.Hashtable;
 
 public class Strings 
 {
-  public static int lengthOfLongestSubstring(String s) 
+  public static int lengthOfLongestSubstring(String s) {return lengthOfLongestSubstring(s,0);}
+  public static int lengthOfLongestSubstring(String s, int longest) 
   {    
     int size = s.length();
     
     if(size < 2) return size;
 
-    int start = 0;
-
     Hashtable<Character,Integer> counter = new Hashtable<>();
-    int maxSize = 0;//longest substr length
+    int maxSize = longest;//longest substr length
     
     for(int i = 0; i < size; i++)
     {
@@ -21,11 +20,13 @@ public class Strings
       
       if(counter.containsKey(current))
       {
+        System.out.println("'" + current + "' repeated");
+        System.out.println("Max size is: " + maxSize);
         maxSize = newMax(maxSize, counter);
         if(i < size - 1)
         {
-          i = counter.get(current);
-          counter = new Hashtable<>();
+          maxSize = lengthOfLongestSubstring( s.substring(counter.get(current) + 1), maxSize);
+          System.out.println(s.substring(counter.get(current) + 1) + ": " + maxSize);
         }
       }
       else
@@ -40,3 +41,11 @@ public class Strings
     return Integer.max(maxSize,count);
   }
 }
+
+
+/**
+ * 
+ * Runtime: 175 ms, faster than 6.40% of Java online submissions for Longest Substring Without Repeating Characters.
+ * Memory Usage: 39.5 MB, less than 8.07% of Java online submissions for Longest Substring Without Repeating Characters.
+ * 
+*/
