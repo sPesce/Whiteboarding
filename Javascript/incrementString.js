147eaf7f1dd5850994abad854f44693f48b5d19d
Steve Pesce <stevepesce879@gmail.com>
2020-11-02 15:23:00 -0500

    working solution/refactored

--- Javascript/incrementString.js
+++ Javascript/incrementString.js
@@ -1,27 +1,17 @@
 function incrementString (strng) {
-  const peices = strng.split(/(\d+$)/);
-  if(peices.length === 1)
-    return peices[0] + "1";
+  const [inpStr,numberStr,] = strng.split(/(\d+$)/);
+  if(numberStr === undefined)
+    return inpStr + "1";
 
-  const [inpStr,numberStr,] = peices;  
   const count = parseInt( numberStr ) + 1;
 
-  if(!numberStr.startsWith('0')) return inpStr + count;
+  if(!numberStr.startsWith('0')) 
+    return inpStr + count;
 
-  const zeroes = zeroCount(numberStr);
-
+  const [,zeroesStr,num] = numberStr.split(/(^0+)/);
+  let zeroCount = zeroesStr.length;
+  if(!num || count % 10 === 0)
+    zeroCount--; 
   
-      
-  
-  
-  
-  zeroes = (numberStr.match(/^0+$/)) ? zeroes - 1 : zeroes;
-  zeroes = (count % 10 === 0) ? zeroes - 1 : zeroes; 
-  
-  return inpStr + '0'.repeat(zeroes) + count;
-}
-
-const zeroCount = numberStr => {
-  const [,zeroes,num] = numberStr.split(/(^0+)/);
-  return !num ? zeroes - 1 : zeroes;
+  return inpStr + '0'.repeat(zeroCount) + count;
 }
