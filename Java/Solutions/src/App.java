eb8ae3770ebc30e5ca12ccf5f49671b31c123ce8
Steve Pesce <stevepesce879@gmail.com>
2020-11-13 16:27:11 -0500

    not working but getting somewhere

--- Java/Solutions/src/App.java
+++ Java/Solutions/src/App.java
@@ -2,39 +2,8 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-      int[][] sudoku = {
-        {5, 3, 4, 6, 7, 8, 9, 1, 2},
-        {6, 7, 2, 1, 9, 5, 3, 4, 8},
-        {1, 9, 8, 3, 4, 2, 5, 6, 7},
-        {8, 5, 9, 7, 6, 1, 4, 2, 3},
-        {4, 2, 6, 8, 5, 3, 7, 9, 1},
-        {7, 1, 3, 9, 2, 4, 8, 5, 6},
-        {9, 6, 1, 5, 3, 7, 2, 8, 4},
-        {2, 8, 7, 4, 1, 9, 6, 3, 5},
-        {3, 4, 5, 2, 8, 6, 1, 7, 9}
-      };
-      
-      System.out.println(SudokuValidator.check(sudoku));
-      
-      sudoku[0][0]++;
-      sudoku[1][1]++;
-      sudoku[0][1]--;
-      sudoku[1][0]--;
-      
-      System.out.println(SudokuValidator.check(sudoku));
-      
-      sudoku[0][0]--;
-      sudoku[1][1]--;
-      sudoku[0][1]++;
-      sudoku[1][0]++;
-      
-      sudoku[4][4] = 0;
-      
-      System.out.println(SudokuValidator.check(sudoku));
-      for(int[] row : sudoku)
-        System.out.println(Arrays.toString(row));
-
-      
+    
+      for(int i = 0; i < 6; System.out.println(i + " - " + i++));
     }
+
   }
--- Java/Solutions/src/Hamming.java
+++ Java/Solutions/src/Hamming.java
@@ -1,7 +1,27 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Hamming {
 
 	public static long hamming(int n) {
-  	
-  }
-  
+    if( n <= 6) return (long)n;
+
+    for(int i = 1; i <= 6; hams.put(i,(long)i++));
+    for(int i = 1; i < n;
+    
+    Map<Integer,Long> hams = new HashMap<Integer,Long>();
+ i++)
+    {
+      if(i <= 6)
+        hams.put(i,(long)i);
+      else
+      {
+
+      }
+    }
+
+    return -1;
+  }  
 }
