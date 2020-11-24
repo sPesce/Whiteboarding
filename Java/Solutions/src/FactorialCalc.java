71e45125a41505dc1742781ac52f620ccaa2a391
Steve Pesce <stevepesce879@gmail.com>
2020-11-24 18:06:45 -0500

    made multiply also recursive

--- Java/Solutions/src/FactorialCalc.java
+++ Java/Solutions/src/FactorialCalc.java
@@ -1,23 +1,17 @@
 public class FactorialCalc {
   public static String Factorial(int n) {
-    if(n > 1) return multiply(n, Factorial(n-1));
+    if(n > 1) return multiply(n, Factorial(n-1),0,new StringBuilder());
     return "1";
   }
-  private static String multiply(int x, String num)
-  {      
-    int overflow = 0;
-    char[] digits = num.toCharArray();
-    StringBuilder sb = new StringBuilder();
-    
-    //multiplication by hand approach
-    for( int j = digits.length - 1; j >= 0; j-- )
-    {
-      //multiplication at current digit
-      final int mult = Character.getNumericValue(digits[j]) * x + overflow;
-      sb.append( Character.forDigit(mult % 10,10) );//append 1s place
-      overflow = mult / 10;
-    }        
-    //overflow added to the front if it exists
-    return (overflow == 0 ? "" : Integer.toString(overflow)) + sb.reverse().toString();
-  }
+  private static String multiply(int x, String num, int overflow, StringBuilder sb)
+ {
+    if(num.length() == 0)
+      return (overflow == 0 ? "" : Integer.toString(overflow)) + sb.reverse().toString();
+   
+    char digit = num.toCharArray()[num.length() - 1];
+    final int product = Character.getNumericValue(digit) * x + overflow;
+    sb.append(Character.forDigit(product % 10, 10));
+
+    return multiply(x, num.substring(0, num.length() - 1), product / 10 , sb);
+ } 
 }
