ada6084d7964f0adc5501ae17d9ef54f2a3ed6e2
Steve Pesce <stevepesce879@gmail.com>
2020-11-24 13:23:09 -0500

    not working but getting somewhere

--- Java/Solutions/src/FactorialCalc.java
+++ Java/Solutions/src/FactorialCalc.java
@@ -1,10 +1,29 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class FactorialCalc {
   public static String Factorial(int n) {
-    return Integer.toString(factorial(n));
-  }
-  private static int factorial(int n){
-    if( n == 1)
-      return 1;
-    return n * factorial(n-1);
+    String num = "1";
+    int overflow = 0;
+    for(int i = 1; i <= n; i++)
+    {
+      char[] digits = num.toCharArray();
+      Deque<Character> stack = new ArrayDeque<Character>();
+      for(int j = digits.length - 1; j >= 0; j --)
+      {
+        final int mult = Character.getNumericValue(digits[j]) * i + overflow;
+        stack.push( Character.forDigit(mult,10) );
+        overflow = mult / 10;
+      }
+      stack.push(Character.forDigit(overflow,10));
+
+      StringBuilder sb = new StringBuilder();
+      while(!stack.isEmpty())
+      {
+        sb.append(stack.pop());
+      }
+      num = sb.toString();
+    }
+    return num;
   }
 }
