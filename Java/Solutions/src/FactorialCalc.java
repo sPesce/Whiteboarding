473bc6f1338a13ec1f17bb73d40da258fe118417
Steve Pesce <stevepesce879@gmail.com>
2020-11-24 15:11:10 -0500

    extracted multiply method

--- Java/Solutions/src/FactorialCalc.java
+++ Java/Solutions/src/FactorialCalc.java
@@ -3,19 +3,23 @@ public class FactorialCalc {
     String num = "1";
     for(int i = 1; i <= n; i++)
     {
-      int overflow = 0;
-      char[] digits = num.toCharArray();
-      StringBuilder sb = new StringBuilder();
-      for(int j = digits.length - 1; j >= 0; j --)
-      {
-        final int mult = Character.getNumericValue(digits[j]) * i + overflow;
-        sb.append( Character.forDigit(mult % 10,10) );
-        overflow = mult / 10;
-      }  
-       
-      //overflow added to the front if it exists
-      num = (overflow == 0 ? "" : Integer.toString(overflow)) + sb.reverse().toString();
+      num = multiply(i,num);
     }
     return num;
+  }
+  private static String multiply(int x, String num)
+  {      
+    int overflow = 0;
+    char[] digits = num.toCharArray();
+    StringBuilder sb = new StringBuilder();
+    for(int j = digits.length - 1; j >= 0; j --)
+    {
+      final int mult = Character.getNumericValue(digits[j]) * x + overflow;
+      sb.append( Character.forDigit(mult % 10,10) );
+      overflow = mult / 10;
+    }  
+      
+    //overflow added to the front if it exists
+    return (overflow == 0 ? "" : Integer.toString(overflow)) + sb.reverse().toString();
   }
 }
