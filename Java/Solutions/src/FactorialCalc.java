219e30ebfa306fd2d2bdfffe33e82a8a4f3583c3
Steve Pesce <stevepesce879@gmail.com>
2020-11-24 15:52:29 -0500

    readme

--- Java/Solutions/src/FactorialCalc.java
+++ Java/Solutions/src/FactorialCalc.java
@@ -1,21 +1,22 @@
 public class FactorialCalc {
   public static String Factorial(int n) {
-    if(n == 1) return "1";
-
-    return multiply(n, Factorial(n-1));
+    if(n > 1) return multiply(n, Factorial(n-1));
+    return "1";
   }
   private static String multiply(int x, String num)
   {      
     int overflow = 0;
     char[] digits = num.toCharArray();
     StringBuilder sb = new StringBuilder();
-    for(int j = digits.length - 1; j >= 0; j --)
+    
+    //multiplication by hand approach
+    for( int j = digits.length - 1; j >= 0; j-- )
     {
+      //multiplication at current digit
       final int mult = Character.getNumericValue(digits[j]) * x + overflow;
-      sb.append( Character.forDigit(mult % 10,10) );
+      sb.append( Character.forDigit(mult % 10,10) );//append 1s place
       overflow = mult / 10;
-    }  
-      
+    }        
     //overflow added to the front if it exists
     return (overflow == 0 ? "" : Integer.toString(overflow)) + sb.reverse().toString();
   }
