1a228aef11050b911784c8cc90020275f27f7cdf
Steve <stevepesce879@gmail.com>
2020-12-18 13:42:56 -0500

    placed c-sharp into its own project, renamed class

---
+++ C-Sharp/C-Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
